5b7ca55a25c072b5536fac1bc69488515097e646
Hosie Owens <hosieo@gmail.com>
2015-10-24 11:23:39 -0400

    Initial revision.

---
+++ hosieoGae3/src/com/hosieo/gae3/HosieoGae3Servlet.java
@@ -0,0 +1,12 @@
+package com.hosieo.gae3;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HosieoGae3Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
