81f1ef55722cc9f06916981fec9551e5dbdb711c
Hosie Owens <hosieo@gmail.com>
2015-10-24 11:42:55 -0400

    Updated

--- hosieoGae3/src/com/hosieo/gae3/HosieoGae3Servlet.java
+++ hosieoGae3/src/com/hosieo/gae3/HosieoGae3Servlet.java
@@ -1,12 +1,14 @@
 package com.hosieo.gae3;
 
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class HosieoGae3Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		resp.getWriter().println("Hello, world. It's " + new Date());
 	}
 }
