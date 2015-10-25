eb9edb59aa63b56144ad2c6bc37d9d59a09667c1
Hosie Owens <hosieo@gmail.com>
2015-10-25 19:21:39 -0400

    Added Bootstrap.

--- hosieoGae3/src/com/hosieo/gae3/HosieoGae3Servlet.java
+++ hosieoGae3/src/com/hosieo/gae3/HosieoGae3Servlet.java
@@ -1,8 +1,16 @@
 package com.hosieo.gae3;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 
@@ -11,8 +19,23 @@ import javax.servlet.http.*;
  * October 25, 2015
  */
 public class HosieoGae3Servlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world. It's " + new Date());
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		SimpleDateFormat fmt = new SimpleDateFormat("MM/dd/yy hh:mm:ss");
+		
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String loginUrl = userService.createLoginURL("/");
+		String logoutUrl = userService.createLogoutURL("/");
+		
+		req.setAttribute("user", user);
+		req.setAttribute("loginUrl", loginUrl);
+		req.setAttribute("logoutUrl", logoutUrl);
+		req.setAttribute("currentTime", fmt.format(new Date()));
+		
+		resp.setContentType("text/html");
+		
+		RequestDispatcher jsp = req.getRequestDispatcher("/WEB-INF/home.jsp");
+		jsp.forward(req, resp);
+		
 	}
 }
---
+++ hosieoGae3/war/WEB-INF/home.jsp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<link rel="stylesheet" src="styles.css" >
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400' rel='stylesheet' type='text/css'>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400' rel='stylesheet' type='text/css'>
+<title>GAE03</title>
+
+</head>
+<body>
+
+	<div class="container-fluid">
+		<h1 style="font-family: Roboto; font-weight: 100;">Home</h1>
+		<pre>The time is ${currentTime}</pre>
+	</div>
+</body>
+</html>
